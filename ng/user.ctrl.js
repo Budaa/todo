22edfe22586d19b5f519f2dbbc69667e19344d42
Budaa <pbuderaski@gmail.com>
2016-05-27 01:48:14 +0100

    sceleton of login method

--- ng/user.ctrl.js
+++ ng/user.ctrl.js
@@ -8,7 +8,7 @@ angular.module('toDo')
 
 
 //REGISTER NEW USER
-	//register errprs array
+	//register errors array
 	$scope.registerError = []
 	$scope.registerUser = function(data) {
 		if(data.password != data.password2){
