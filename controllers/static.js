6f0f4094833d22a789b5df923a64ee1a2c09ff65
Budaa <pbuderaski@gmail.com>
2016-06-29 22:25:34 +0100

    Remove unnesesary route

--- controllers/static.js
+++ controllers/static.js
@@ -7,9 +7,6 @@ router.use(express.static(__dirname + '/../templates'))
 router.get('/', function(req, res) {
 	res.sendfile('layouts/app.html')
 })
-router.get('/fb', function(req, res) {
-	res.sendfile('fb.html')
-})
 
 router.get('/page', function(req, res, next) {
 	res.sendfile('page.html', function(err) {
